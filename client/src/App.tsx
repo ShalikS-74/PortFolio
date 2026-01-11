f6eb331c92eb19884a5bb1ae93e2dd28e06ed13b
ShalikS-74 <shaliksahul74@gmail.com>
2026-01-12 00:32:07 +0530

    Remove unused Radix UI toast dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Switch, Route } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
@@ -16,7 +15,6 @@ function Router() {
 function App() {
   return (
     <>
-      <Toaster />
       <Router />
     </>
   );
